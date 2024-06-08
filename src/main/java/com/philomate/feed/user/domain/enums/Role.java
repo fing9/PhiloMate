b4ba1749e3ea20c3da0f64c0a34edf434ea6f519
fing9 <djajsl54@naver.com>
2024-06-08 18:33:20 +0900

    [Fix] 유저 Role 관련 버그 수정

--- src/main/java/com/philomate/feed/user/domain/enums/Role.java
+++ src/main/java/com/philomate/feed/user/domain/enums/Role.java
@@ -1,6 +1,22 @@
 package com.philomate.feed.user.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER, ADMIN
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String name;
+
+    public static Role from(String s) {
+        return Role.valueOf(s.toUpperCase());
+    }
+
+    public static Role fromName(String s) {
+        return Role.valueOf(s.substring(5).toUpperCase());
+    }
 
 }
